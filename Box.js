3f60ffeb0a7d3bddc9e824e7c85a71c917ac5f5b
ARJUN2009 <68690799+ARJUN-ILANGO@users.noreply.github.com>
2020-09-18 16:33:45 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,33 @@
+class Box extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+        this.Visibility = 255
+        World.add(world, this.body);
+      }
+
+      
+      display(){
+
+        if(this.body.speed<3){
+        super.display();
+        
+     } else {
+       World.remove(world,this.body)
+        push();
+        this.Visibility = this.Visibility - 2
+        tint(255,this.Visibility)
+        rect(0,0, this.width, this.height);
+        pop();
+      }
+     }
+
+     Score(){
+      if(this.body.speed<3){
+     score = score+3
+      }
+      else{
+        score = score
+      }
+    }
+   };
+    
